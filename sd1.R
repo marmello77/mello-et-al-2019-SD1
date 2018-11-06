14b0c08bec4f8dd55427e6f00f1eea78faf95b3c
Marco Mello <marmello@gmail.com>
2018-11-06 13:23:58 -0200

    Update sd1.R

--- sd1.R
+++ sd1.R
@@ -13,7 +13,7 @@
 
 ##### Updated on October 25th, 2018 (English version).
 
-##### Run in R 3.5.1 (2017-07-02) -- "Feather Spray"
+##### Run R version 3.5.1 (2018-07-02) -- "Feather Spray"
 
 ##### Disclaimer: You may use this script freely for non-comercial purposes at your own risk. We assume no responsibility or liability for the use of this software, convey no license or title under any patent, copyright, or mask work right to the product. We reserve the right to make changes in the software without notification. We also make no representation or warranty that such application will be suitable for the specified use without further testing or modification. If this script helps you produce any academic work (paper, book, chapter, dissertation etc.), please acknowledge the authors and cite the source.
 
